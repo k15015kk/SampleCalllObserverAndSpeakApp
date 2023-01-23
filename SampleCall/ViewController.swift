3c954b911806fe28e08b095ca086dc38fe4d04e3
inoueharuki <inoue.haruki@multisoup.co.jp>
2023-01-23 18:53:35 +0900

    Initial Commit

---
+++ SampleCall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleCall
+//
+//  Created by Inoue Haruki on 2023/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
